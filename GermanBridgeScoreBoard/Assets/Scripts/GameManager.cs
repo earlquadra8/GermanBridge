c75c1ee9d5ff3998a9fadccbb0438a9b8027487c
Earl.cy.Li <earl.cy.li@gmail.com>
2018-09-25 14:47:28 +0800

    Implemant PlayerBehaviour and GameManager basic method

---
+++ GermanBridgeScoreBoard/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public List<PlayerBehaviour> players;
+
+    private int roundNum = 1;
+
+    #region Unity Mono
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Get Set Functions
+    public int GetCurrRoundNum()
+    {
+        return roundNum;
+    }
+    #endregion
+
+    #region OnBtnClick
+    public void OnGameStartBtnClick()
+    {
+
+    }
+
+    public void OnRoundStartBtnClick()
+    {
+        
+    }
+
+    public void OnRoundFinishBtnClick()
+    {
+        foreach (var player in players)
+        {
+            player.SetPlayerWinTrick();
+        }
+
+        roundNum++;
+    }
+    #endregion
+}
---
+++ GermanBridgeScoreBoard/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public class PlayerRound
+    {
+        public int playerId;
+        public int bid;
+        public int win;
+        public int deltaScore;
+
+        public PlayerRound(int playerId, int bid)
+        {
+            this.playerId = playerId;
+            this.bid = bid;
+            this.win = 0;
+            this.deltaScore = 0;
+        }
+    }
+
+    public InputField inputFieldName;
+    public InputField inputFieldBidTrick;
+    public InputField inputFieldWinTrick;
+
+    public GameObject setupConfirmBtn;
+
+    private int playerId;
+    private string playerName;
+    private int playerCurrBid;
+
+    private Stack<PlayerRound> playerRounds = new Stack<PlayerRound>();
+
+    #region Unity Mono
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+    #endregion
+
+    #region Get Set Functions
+    public void SetPlayerId (int playerId)
+    {
+        this.playerId = playerId;
+    }
+
+    public PlayerRound GetCurrPlayerRound ()
+    {
+        return playerRounds.Peek();
+    }
+
+    public void SetPlayerWinTrick()
+    {
+        GetCurrPlayerRound().win = Int32.Parse(inputFieldWinTrick.text);
+    }
+    #endregion
+
+    #region OnBtnClick
+    public void OnPlayerSetUpBtnClick ()
+    {
+        playerName = inputFieldName.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            setupConfirmBtn.SetActive(false);
+            // Setup playerId
+        }
+    }
+
+    public void OnPlayerBidBtnClick ()
+    {
+        PlayerRound playerRound = new PlayerRound(this.playerId, Int32.Parse(inputFieldBidTrick.text));
+        playerRounds.Push(playerRound);
+        // Show score board
+    }
+
+    //public void OnPlayerWinBtnClick ()
+    //{
+    //    playerRounds.Peek().win = Int32.Parse(inputFieldWinTrick.text);
+    //    // Update score board
+    //}
+    #endregion
+}
